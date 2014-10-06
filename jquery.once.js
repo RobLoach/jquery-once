719639d2e7db76b4f043c6e4b2e5b19bb53c600e
Rob Loach <robloach@gmail.com>
2014-10-05 22:29:49 -0400

    Clean up code documentation

--- jquery.once.js
+++ jquery.once.js
@@ -3,17 +3,6 @@
  * @description Act on jQuery elements only once.
  * @version 2.0.0-alpha.5
  * @link http://github.com/robloach/jquery-once
- *
- * @example
- * // Change the color to green only once.
- * $('p').once('changecolor', function() {
- *   $(this).css('color', 'green');
- * });
- *
- * @see once
- * @see removeOnce
- * @see findOnce
- *
  * @author Rob Loach (http://robloach.net)
  * @license MIT, GPL-2.0
  */
@@ -52,6 +41,17 @@
    *   element in the set, while returning false will entirely break the
    *   iteration.
    * @returns jQuery element collection of elements that have now run once.
+   *
+   * @example
+   * // Change the color to green only once.
+   * $('p').once('changecolor', function() {
+   *   // This function is called for every p element that hasn't been called
+   *   // with "changecolor" once() before.
+   *   $(this).css('color', 'green');
+   * });
+   *
+   * @see removeOnce
+   * @see findOnce
    *
    * @public
    * @global
@@ -92,8 +92,17 @@
    *   follows the same logic as $.each(). Returning true will continue to the
    *   next matched element in the set, while returning false will entirely
    *   break the iteration.
+   *
    * @returns jQuery element collection of elements that now have their once
    *   data removed.
+   *
+   * @example
+   * // Remove once data with the "changecolor" ID.
+   * $('p').removeOnce('changecolor', function() {
+   *   // This function is called for all elements that had their once removed.
+   * });
+   *
+   * @see once
    *
    * @public
    * @global
@@ -122,7 +131,16 @@
    *   logic as $.each(). Returning true will continue to the next matched
    *   element in the set, while returning false will entirely break the
    *   iteration.
+   *
    * @returns jQuery element collection of elements that have been run once.
+   *
+   * @example
+   * // Find all elements that have the changecolor'ed once.
+   * $('p').findOnce('changecolor', function() {
+   *   // This function is called for all elements that has already once'd.
+   * });
+   *
+   * @see once
    *
    * @public
    * @global
