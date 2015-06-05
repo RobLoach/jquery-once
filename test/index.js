591a3ecd3ab34011c95370224a5845261217ce2e
Rob Loach <robloach@gmail.com>
2015-06-05 09:39:14 -0400

    Add Coverage with Istanbul

---
+++ test/index.js
@@ -0,0 +1,18 @@
+/**
+ * Mocha test runner.
+ */
+var Mocha = require('mocha');
+var path = require('path');
+ 
+// Create our mocha instance.
+var mocha = new Mocha;
+ 
+// Add all the test files.
+mocha.addFile(path.join(__dirname, 'test.js'));
+ 
+// Run the tests.
+mocha.run(function(failures){
+  process.on('exit', function () {
+    process.exit(failures);
+  });
+});
